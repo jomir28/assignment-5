c694c156f2d908b74f3efd5877e534ab12d265d5
unknown <jomirhossain15@gmail.com>
2024-02-18 23:11:56 +0600

    javascript start

---
+++ test.js
@@ -0,0 +1,59 @@
+"use strict"
+let seatCounts = 0;
+const seats = document.getElementsByClassName('lg:w-[100px]');
+for (const seat of seats) {
+    seat.addEventListener('click', function () {
+        const seatCount = document.getElementById('seat-count');
+        seatCounts++;
+        seatCount.innerText = seatCounts;
+        //   document.getElementById('seat-count').innerText=seatCounts;
+        const totalSeat = document.getElementById('total-seat').innerText;
+        const convertedTotalSeat = parseInt(totalSeat);
+        // console.log(convertedTotalSeat)
+        const newTotalSeat = convertedTotalSeat - 1;
+        document.getElementById('total-seat').innerText = newTotalSeat;
+        // ...........seat count jug biyug complete............... //
+
+        // ...........price calculate ............//
+        const seatPrices = document.getElementById('ticket-price');
+        const seatPricesText =seatPrices.innerText
+        const seatPrice = parseInt(seatPricesText);
+
+        const totalPrices = document.getElementById('total-price');
+        const totalPricesText = totalPrices.innerText;
+        const totalPrice = parseInt(totalPricesText);
+        const confirmTotalPrice =seatPrice + totalPrice;
+        totalPrices.innerText = confirmTotalPrice;
+
+        const grandTotals = document.getElementById('grand-total');
+        const grandTotalsText =grandTotals.innerText;
+        const grandTotal = parseInt(grandTotalsText);
+        grandTotals.innerText=seatPrice+grandTotal;
+        
+        // ............................................................... // 
+        // chesta kore dekhi cupon apply discount korte pari kina //
+     
+
+
+
+
+    })
+}
+
+
+document.getElementById('apply-btn').addEventListener('click',function(){
+    
+ const cuponField =document.getElementById('cupun-field').value;
+ if(cuponField === "Couple 20"){
+
+    const grandTotal =document.getElementById('grand-total');
+    const grandTotalValue =parseInt(grandTotal.innerText);
+    grandTotal.innerText=grandTotalValue-(grandTotalValue*0.2);
+    console.log(grandTotal)
+    
+ }
+ else{
+     alert("Invalid Cupon Code")
+ }
+
+   })
