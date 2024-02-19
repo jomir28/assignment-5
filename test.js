6c590144be87690f4fd2f2b3d9377710979fcc2a
unknown <jomirhossain15@gmail.com>
2024-02-19 14:17:15 +0600

    customized

--- test.js
+++ test.js
@@ -1,8 +1,19 @@
 "use strict"
 let seatCounts = 0;
+let bgCount = 0;
 const seats = document.getElementsByClassName('lg:w-[100px]');
 for (const seat of seats) {
     seat.addEventListener('click', function () {
+
+        if (bgCount <4){
+            bgCount =bgCount + 1;
+            seat.classList.add("bg-green-500")
+        }
+        
+        else{
+            alert("you buy enough seat")
+        }
+
         const seatCount = document.getElementById('seat-count');
         seatCounts++;
         seatCount.innerText = seatCounts;
@@ -15,37 +26,37 @@ for (const seat of seats) {
 
         // ...........price calculate ............//
         const seatPrices = document.getElementById('ticket-price');
-        const seatPricesText =seatPrices.innerText
+        const seatPricesText = seatPrices.innerText
         const seatPrice = parseInt(seatPricesText);
 
         const totalPrices = document.getElementById('total-price');
         const totalPricesText = totalPrices.innerText;
         const totalPrice = parseInt(totalPricesText);
-        const confirmTotalPrice =seatPrice + totalPrice;
+        const confirmTotalPrice = seatPrice + totalPrice;
         totalPrices.innerText = confirmTotalPrice;
 
         const grandTotals = document.getElementById('grand-total');
-        const grandTotalsText =grandTotals.innerText;
+        const grandTotalsText = grandTotals.innerText;
         const grandTotal = parseInt(grandTotalsText);
-        grandTotals.innerText=seatPrice+grandTotal;
-        
+        grandTotals.innerText = seatPrice + grandTotal;
+
         // ............................................................... // 
         // chesta kore dekhi append child er section korte pari kina //
-        const targetText =event.target.innerText;
+        const targetText = event.target.innerText;
         console.log(targetText)
-        const appendContainer =document.getElementById('append-container');
-        // console.log(appendContainer)
-        const p1 =document.createElement("p");
-        p1.innerText=targetText;
-
-        const p2 =document.createElement("p");
-        p2.innerText="Economoy"
-
-        const p3 =document.createElement("p");
-        p3.innerText=seatPrice;
-
-        const div =document.createElement("div")
         
+        const appendContainer = document.getElementById('append-container');
+        const p1 = document.createElement("p");
+        p1.innerText = targetText;
+
+        const p2 = document.createElement("p");
+        p2.innerText = "Economoy"
+
+        const p3 = document.createElement("p");
+        p3.innerText = seatPrice;
+
+        const div = document.createElement("div")
+
         div.appendChild(p1);
         div.appendChild(p2);
         div.appendChild(p3);
@@ -54,9 +65,6 @@ for (const seat of seats) {
         console.log(div);
         appendContainer.appendChild(div);
         // .................................. //
-     
-
-
 
 
     })
@@ -64,37 +72,39 @@ for (const seat of seats) {
 
 
 
+
+
 // discount cupoon section ///
-document.getElementById('apply-btn').addEventListener('click',function(){
-    
- const cuponField =document.getElementById('cupun-field').value;
- if(cuponField === "Couple 20"){
+document.getElementById('apply-btn').addEventListener('click', function () {
 
-    const grandTotal =document.getElementById('grand-total');
-    const grandTotalValue =parseInt(grandTotal.innerText);
-    grandTotal.innerText=grandTotalValue-(grandTotalValue*0.2);
-    // console.log(grandTotal)
+    const cuponField = document.getElementById('cupun-field').value;
+    if (cuponField === "Couple 20") {
 
-    const inputField =document.getElementById('input-field');
-    inputField.classList.add('hidden');
-    // modal //
-const showModal =document.getElementById('next-btn');
-showModal.addEventListener('click', function(){
-    my_modal_1.showModal();
+        const grandTotal = document.getElementById('grand-total');
+        const grandTotalValue = parseInt(grandTotal.innerText);
+        grandTotal.innerText = grandTotalValue - (grandTotalValue * 0.2);
+        // console.log(grandTotal)
+
+        const inputField = document.getElementById('input-field');
+        inputField.classList.add('hidden');
+        // modal //
+        const showModal = document.getElementById('next-btn');
+        showModal.addEventListener('click', function () {
+            my_modal_1.showModal();
+        })
+
+    }
+    else if (cuponField === "NEW15") {
+        const grandTotal = document.getElementById('grand-total');
+        const grandTotalValue = parseInt(grandTotal.innerText);
+        grandTotal.innerText = grandTotalValue - (grandTotalValue * 0.15);
+
+        const inputField = document.getElementById('input-field');
+        inputField.classList.add('hidden')
+    }
+    else {
+        alert("Invalid Cupon Code");
+        document.getElementById('cupun-field').value = "";
+    }
+
 })
-    
- }
- else if(cuponField === "NEW15"){
-    const grandTotal =document.getElementById('grand-total');
-    const grandTotalValue =parseInt(grandTotal.innerText);
-    grandTotal.innerText=grandTotalValue-(grandTotalValue*0.15);
-
-    const inputField =document.getElementById('input-field');
-    inputField.classList.add('hidden')
- }
- else{
-     alert("Invalid Cupon Code");
-     document.getElementById('cupun-field').value="";
- }
-
-   })
